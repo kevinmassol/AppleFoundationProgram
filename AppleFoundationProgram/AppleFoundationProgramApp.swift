c3673c4d7b60231e11197528f51dddc282a81c9a
Kevin Massol <kevin.massol@icloud.com>
2024-01-02 21:41:57 +0100

    Initial Commit

---
+++ AppleFoundationProgram/AppleFoundationProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFoundationProgramApp.swift
+//  AppleFoundationProgram
+//
+//  Created by KEVIN on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFoundationProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
